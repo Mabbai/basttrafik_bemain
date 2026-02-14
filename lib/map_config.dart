dc3df640116cb44eb178fc8dff5cb195fe00ca6b
Mattias Smedstad <matsme@chalmers.com>
2026-02-14 17:43:08 +0100

    fixed const issue

--- lib/map_config.dart
+++ lib/map_config.dart
@@ -3,13 +3,25 @@ import 'dart:io';
 
 import 'package:flutter/widgets.dart';
 
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
 class MapConfig {
-  const MapConfig({required this.imageSize, required this.stationsSize}) : scale = imageSize.width / stationsSize.width;
+  // 1. Keep the constructor const, but remove 'scale' from it
+  const MapConfig({
+    required this.imageSize, 
+    required this.stationsSize
+  });
 
   final Size imageSize;
   final Size stationsSize;
-  final double scale;
 
+  // 2. Turn 'scale' into a getter. 
+  // This moves the math from "Compile-time" to "Runtime"
+  double get scale => imageSize.width / stationsSize.width;
+
+  // 3. Now this 'const' is valid because there is no math inside the constructor
   static const MapConfig fallback = MapConfig(
     imageSize: Size(10760, 13146),
     stationsSize: Size(911, 1113),
